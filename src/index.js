92b082f159720401addf41d483eba18784bb3240
zhangxiaorong <zhangxiaorong@mia.com>
2020-07-14 10:46:40 +0800

    删除不用文件

--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App'
-// import Example from './Example'
-
-ReactDOM.render(<App />, document.getElementById('root'))
